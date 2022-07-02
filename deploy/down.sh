3819205ef1e7964d87ef3910e8a3304865fc248b
juhonamnam <jhnamnam@gmail.com>
2022-07-02 23:52:51 +0900

    modify reading env.sh

--- deploy/down.sh
+++ deploy/down.sh
@@ -1,5 +1,8 @@
 # env
-. ./env.sh
+if [[ -f ./env.sh ]]; then
+    echo "getting environment variables from env.sh..."
+    . ./env.sh
+fi
 
 docker stop $(docker ps -q --filter name=$CONTAINER_NAME)
 docker rm $(docker ps -a -q --filter name=$CONTAINER_NAME)
--- deploy/up.sh
+++ deploy/up.sh
@@ -1,7 +1,11 @@
 # env
-. ./env.sh
+if [[ -f ./env.sh ]]; then
+    echo "getting environment variables from env.sh..."
+    . ./env.sh
+fi
 
-. ./deploy/down.sh
+docker stop $(docker ps -q --filter name=$CONTAINER_NAME)
+docker rm $(docker ps -a -q --filter name=$CONTAINER_NAME)
 
 mkdir -p mysql
 docker run \
